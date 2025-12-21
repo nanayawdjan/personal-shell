45166326e9b0f4ff7a9823bd7656d9a8d896d0dc
kingsloe <nanayawdjan447@gmail.com>
2025-12-21 10:32:00 -0800

    captured user input

--- app/main.py
+++ app/main.py
@@ -3,8 +3,9 @@ import time
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    command = input()
+    sys.stdout.write(f"{command}: command not found")
 
 
 if __name__ == "__main__":
