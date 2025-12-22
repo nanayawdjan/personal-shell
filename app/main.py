c45c1f4059f08c32f818efbe69edfce2a4d7365c
kingsloe <nanayawdjan447@gmail.com>
2025-12-21 20:15:01 -0800

    Add search path

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import shutil
 
 def command_type(*args):
     if not args:
@@ -7,31 +8,32 @@ def command_type(*args):
     elif args and args[0] in commands:
         print(f"{args[0]} is a shell builtin")
     else: 
-        print(f"{args[0]}: not found")
+        if shutil.which(args[0]):
+            print(f"{args[0]} is {shutil.which(args[0])}")
+        else:
+            print(f"{args[0]}: not found")
 
 commands = {
     "echo": lambda *args: print(' '.join(args)),
-    "exit": None,
+    "exit": lambda *args: sys.exit(),
     "type": command_type
 }
 
 
 def main():
     
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command_with_args = input().split()
+        # Wait for user input
+        command_with_args = input().split()
 
-    command = command_with_args[0]
+        command = command_with_args[0]
 
-    if command not in commands:
-        print(f"{command}: command not found")
-    elif command == "exit":
-        sys.exit()
-    else:
-        commands[command](*command_with_args[1:])
-    main()
+        if command not in commands:
+            print(f"{command}: command not found")
+        else:
+            commands[command](*command_with_args[1:])
 
 
 if __name__ == "__main__":
