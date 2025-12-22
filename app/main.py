c3c7440b70d1b3897fcee3fc2bc8c60f54c0fe3c
kingsloe <nanayawdjan447@gmail.com>
2025-12-21 16:55:06 -0800

    Add type feature

--- app/main.py
+++ app/main.py
@@ -1,19 +1,36 @@
 import sys
 import time
 
+def command_type(*args):
+    if not args:
+        print("type: missing operand")
+    elif args and args[0] in commands:
+        print(f"{args[0]} is a shell builtin")
+    else: 
+        print(f"{args[0]}: not found")
+
+commands = {
+    "echo": lambda *args: print(' '.join(args)),
+    "exit": None,
+    "type": command_type
+}
+
 
 def main():
     
     sys.stdout.write("$ ")
 
     # Wait for user input
-    command = input()
-    if command == "exit":
+    command_with_args = input().split()
+
+    command = command_with_args[0]
+
+    if command not in commands:
+        print(f"{command}: command not found")
+    elif command == "exit":
         sys.exit()
-    elif command.split()[0] == "echo":
-        print(' '.join(command.split()[1:]))
     else:
-        sys.stdout.write(f"{command}: command not found\n")
+        commands[command](*command_with_args[1:])
     main()
 
 
