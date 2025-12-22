0dd2b00e2e8f1c2eff384b080bc8485dfc6c8f43
kingsloe <nanayawdjan447@gmail.com>
2025-12-21 21:17:11 -0800

    Add search run external file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+import os
 import sys
 import time
 import shutil
+import subprocess
 
 def command_type(*args):
     if not args:
@@ -12,6 +14,14 @@ def command_type(*args):
             print(f"{args[0]} is {shutil.which(args[0])}")
         else:
             print(f"{args[0]}: not found")
+
+def find_command(*args):
+    full_path = shutil.which(args[0])
+    if full_path:
+        subprocess.run([full_path])
+    else:
+        print(f"{args[0]}: command not found")
+
 
 commands = {
     "echo": lambda *args: print(' '.join(args)),
@@ -31,7 +41,7 @@ def main():
         command = command_with_args[0]
 
         if command not in commands:
-            print(f"{command}: command not found")
+            find_command(command)
         else:
             commands[command](*command_with_args[1:])
 
